8449233234173506993a5f0289265934b5dc7dd9
Denchik23083 <deniskudravov228@gmail.com>
2021-12-08 21:03:47 +0200

    statistics

--- src/app/components/user/statistics/statistics.component.ts
+++ src/app/components/user/statistics/statistics.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { StatisticsModel, UserService } from 'src/app/services/user.service';
+import { BehaviorSubject } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-statistics',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatisticsComponent implements OnInit {
 
-  constructor() { }
+  statistic: StatisticsModel = {
+    name: '',
+    point: 0,
+  };
+
+  statistics$!: BehaviorSubject<StatisticsModel[]>;
+
+  constructor(private service: UserService, private router: Router) {
+    this.statistics$ = service.statistics$;
+  }
 
   ngOnInit(): void {
+    this.service.getPoints().subscribe();
   }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -10,12 +10,20 @@ export interface UserModel{
   password: string,
 }
 
+export interface StatisticsModel{
+  name: string,
+  point: number,
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   apiLink = 'https://localhost:5001/api';
+  statisticsLink = 'https://localhost:6001/api/Quiz';
+
   user$ = new BehaviorSubject<UserModel[]>([]);
+  statistics$ = new BehaviorSubject<StatisticsModel[]>([]);
 
   constructor(private http: HttpClient) { }
 
@@ -23,6 +31,13 @@ export class UserService {
     return this.http.get<UserModel[]>(`${this.apiLink}/User`)
       .pipe(
         tap(user => this.user$.next(user))
+      );
+  }
+
+  getPoints(): Observable<StatisticsModel[]>{
+    return this.http.get<StatisticsModel[]>(this.statisticsLink)
+      .pipe(
+        tap(books => this.statistics$.next(books))
       );
   }
 }
