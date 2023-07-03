376c0dd64530c5970837249b6fe630f546f87c5d
Denchik23083 <deniskudravov228@gmail.com>
2023-07-03 17:47:40 +0300

    create pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthComponent } from './components/auth/auth/auth.component';
 import { GameComponent } from './components/brain/game/game.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { QuizzesComponent } from './components/brain/quizzes/quizzes.component';
@@ -9,13 +8,14 @@ import { RegisterComponent } from './components/auth/register/register.component
 import { StatisticsComponent } from './components/user/statistics/statistics.component';
 import { EditComponent } from './components/user/edit/edit.component';
 import { DeleteComponent } from './components/user/delete/delete.component';
-import { MainComponent } from './components/utilities/main/main.component';
+import { MainComponent } from './components/main/main.component';
 import { PointsComponent } from './components/brain/points/points.component';
 import { NewComponent } from './components/brain/new/new.component';
 import { PasswordComponent } from './components/user/password/password.component';
+import { AuthPageComponent } from './pages/auth/auth-page/auth-page.component';
 
 const routes: Routes = [
-  { path: '', component: AuthComponent},
+  { path: '', component: AuthPageComponent},
   { path: 'main', component: MainComponent},
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
