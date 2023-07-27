0c5b7ebd221e83589a91de66dbadbe87bb7ec2f2
Denchik23083 <deniskudravov228@gmail.com>
2023-07-27 17:25:19 +0300

    crud quiz is done

--- src/app/components/brain/update-quizzes/update-quizzes.component.ts
+++ src/app/components/brain/update-quizzes/update-quizzes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { QuizService, QuizzesModel } from 'src/app/services/quiz/quiz.service';
 
 @Component({
   selector: 'app-update-quizzes',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateQuizzesComponent implements OnInit {
 
-  constructor() { }
+  quiz: QuizzesModel = {
+    id: 0,
+    name: '',
+  };
+
+  constructor(private readonly service: QuizService, private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  submit(form: NgForm): void {
+    const id = this.activatedRoute.snapshot.paramMap.get('id') as any
+
+    const updateQuiz = form.value as QuizzesModel;
+    
+    this.service.update(updateQuiz, id).subscribe(() => {
+      this.router.navigate(['/quizzes']);
+    });
   }
 
 }
